ba0d43f5d0ccf0ca630b2f68a8b3b2bd733f3785
Jan Riehn <jan.riehn@web.de>
2015-06-16 21:44:29 +0200

    first chat functionality

--- Moody/Moody/ViewController.swift
+++ Moody/Moody/ViewController.swift
@@ -1,17 +1,55 @@
 import UIKit
+import Socket_IO_Client_Swift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet var chatView: UITableView!
+    @IBOutlet var textField: UITextField!
+    
+    let socket = SocketIOClient(socketURL: "localhost:3000")
+    var chatItems: [String] = []
+    
+    func addHandlers() {
+        self.socket.on("chat message") {[weak self] data, ack in
+            self!.chatItems.append(data!.firstObject! as! String)
+            self!.chatView.reloadData()
+            self!.textField.text = ""
+            return
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addHandlers()
+        socket.connect()
+        
+        chatView.dataSource = self
+        chatView.delegate = self
+        
+        textField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onSubmit(sender: AnyObject) {
+        self.socket.emit("chat message", self.textField.text)
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("chatCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel!.text = chatItems[indexPath.item]
+        return cell
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.socket.emit("chat message", self.textField.text)
+        
+        return true
+    }
+}
